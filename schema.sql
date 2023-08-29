aeb45d6dfc3c2b8b0d6d6e85cfd3f54a01455024
devendra-alt <devendramulewa171@gmail.com>
2023-08-29 15:52:45 +0530

    Update id to Serial Primary key type on animals table

--- schema.sql
+++ schema.sql
@@ -3,4 +3,10 @@
 
 CREATE TABLE animals(id SERIAL , name CHAR(50), date_of_birth DATE, escape_attempts INT, neutered BOOLEAN, weight_kg DECIMAL);
 
+ALTER TABLE animals ADD COLUMN new_id SERIAL PRIMARY KEY;
+
+ALTER TABLE animals DROP COLUMN id;
+
+ALTER TABLE animals RENAME COLUMN new_id TO id;
+
 ALTER TABLE animals ADD COLUMN species CHAR(50);
